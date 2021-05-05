6ae8d7eebfa75f7028c84587c8842dc695bb1403
Kohei Tabata <nerd0geek1@gmail.com>
2021-05-05 21:47:06 +0900

    update SwiftFlutterHapticPlugin logic

--- ios/Classes/SwiftFlutterHapticPlugin.swift
+++ ios/Classes/SwiftFlutterHapticPlugin.swift
@@ -2,6 +2,12 @@ import Flutter
 import UIKit
 
 public class SwiftFlutterHapticPlugin: NSObject, FlutterPlugin {
+
+    // MARK: - private properties
+
+    private lazy var notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+    private lazy var selectionFeedbackGenerator    = UISelectionFeedbackGenerator()
+
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "flutter_haptic", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterHapticPlugin()
@@ -9,6 +15,15 @@ public class SwiftFlutterHapticPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    switch call.method {
+    case "performSuccessFeedback":
+        notificationFeedbackGenerator.notificationOccurred(.success)
+    case "performFailureFeedback":
+        notificationFeedbackGenerator.notificationOccurred(.error)
+    default:
+        result(FlutterMethodNotImplemented)
+        fatalError()
+    }
+    result(nil)
   }
 }
