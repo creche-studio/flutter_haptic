a848745a9fa73e007794bf9ad9e10eebba71112a
Kohei Tabata <nerd0geek1@gmail.com>
2021-05-08 21:41:24 +0900

    fix code indent of SwiftFlutterHapticPlugin.swift

--- ios/Classes/SwiftFlutterHapticPlugin.swift
+++ ios/Classes/SwiftFlutterHapticPlugin.swift
@@ -8,22 +8,22 @@ public class SwiftFlutterHapticPlugin: NSObject, FlutterPlugin {
     private lazy var notificationFeedbackGenerator = UINotificationFeedbackGenerator()
     private lazy var selectionFeedbackGenerator    = UISelectionFeedbackGenerator()
 
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_haptic", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterHapticPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "performSuccessFeedback":
-        notificationFeedbackGenerator.notificationOccurred(.success)
-    case "performFailureFeedback":
-        notificationFeedbackGenerator.notificationOccurred(.error)
-    default:
-        result(FlutterMethodNotImplemented)
-        fatalError()
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "flutter_haptic", binaryMessenger: registrar.messenger())
+        let instance = SwiftFlutterHapticPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-    result(nil)
-  }
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "performSuccessFeedback":
+            notificationFeedbackGenerator.notificationOccurred(.success)
+        case "performFailureFeedback":
+            notificationFeedbackGenerator.notificationOccurred(.error)
+        default:
+            result(FlutterMethodNotImplemented)
+            fatalError()
+        }
+        result(nil)
+    }
 }
