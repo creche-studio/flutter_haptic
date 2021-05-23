1f88038146a17cfcc7bf51826dd7150611798dd8
Kohei Tabata <nerd0geek1@gmail.com>
2021-05-24 08:45:31 +0900

    update minSdkVersion from 26 to 23, fix related code

--- android/src/main/kotlin/studio/creche/flutter_haptic/FlutterHapticPlugin.kt
+++ android/src/main/kotlin/studio/creche/flutter_haptic/FlutterHapticPlugin.kt
@@ -1,6 +1,5 @@
 package studio.creche.flutter_haptic
 
-import android.content.Context
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
@@ -31,25 +30,35 @@ class FlutterHapticPlugin: FlutterPlugin, MethodCallHandler {
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     when (call.method) {
       "performSuccessFeedback" -> {
-        val effect = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-          VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK)
-        } else {
-          VibrationEffect.createWaveform(longArrayOf(7, 100), intArrayOf(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          val effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_CLICK)
+          vibrator.vibrate(effect)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          val effect = VibrationEffect.createWaveform(longArrayOf(7, 100), intArrayOf(
             VibrationEffect.DEFAULT_AMPLITUDE, 0), -1)
+          vibrator.vibrate(effect)
+        } else {
+          val longArray = longArrayOf(7, 10)
+          vibrator.vibrate(longArray, -1)
         }
-        vibrator.vibrate(effect)
+
         result.success(null)
       }
       "performFailureFeedback" -> {
-        val effect = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-          VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK)
-        } else {
-          VibrationEffect.createWaveform(longArrayOf(9, 130, 11), intArrayOf(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+          val effect = VibrationEffect.createPredefined(VibrationEffect.EFFECT_DOUBLE_CLICK)
+          vibrator.vibrate(effect)
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+          val effect = VibrationEffect.createWaveform(longArrayOf(9, 130, 11), intArrayOf(
             VibrationEffect.DEFAULT_AMPLITUDE, 0,
             VibrationEffect.DEFAULT_AMPLITUDE
           ), -1)
+          vibrator.vibrate(effect)
+        } else {
+          val longArray = longArrayOf(90, 10, 110, 10)
+          vibrator.vibrate(longArray, -1)
         }
-        vibrator.vibrate(effect)
+
         result.success(null)
       }
       else -> {
